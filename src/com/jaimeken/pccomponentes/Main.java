fde3c86c17826b72c00c24f2d5f29d4e2ddd93dc
jaimeken <jaime.ayuala.1@educa.jcyl.es>
2022-10-17 18:42:52 +0200

    comiwnzo del proyecto

---
+++ src/com/jaimeken/pccomponentes/Main.java
@@ -0,0 +1,7 @@
+package com.jaimeken.pccomponentes;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/com/jaimeken/pccomponentes/Procesador.java
@@ -0,0 +1,21 @@
+package com.jaimeken.pccomponentes;
+
+public class Procesador {
+    private Integer id;
+    private String marca;
+    private String fabricante;
+
+    public void setId(Integer id) {
+        this.id=id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setMarca
+
+
+
+
+}
