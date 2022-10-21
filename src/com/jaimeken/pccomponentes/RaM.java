449753113c59ca5bc7cf966868cf4ac821148120
jaime <jaimeken@yahoo.es>
2022-10-21 15:50:53 +0200

    21-octubre,despues de una muy buena mano de Enrique

---
+++ src/com/jaimeken/pccomponentes/RaM.java
@@ -0,0 +1,20 @@
+package com.jaimeken.pccomponentes;
+public class RaM{
+
+private Integer id;
+private String marca;
+private String comptibilidad;
+private String frecuencia;
+
+
+    public Integer getID() {return id;}
+    public void setID(Integer id){this.id=id;}
+
+    public String getMarca() {return marca;}
+    public void setMarca(String marca){this.marca=marca;}
+
+    public Integer getCompatibilidad() {return getCompatibilidad();}
+    public void setCompatibilidad(String compatibilidad){this.compatibilidad = compatibilidad;}
+    public String getFrecuencia() {return frecuencia;}
+    public void setFrecuencia(String Frecuencia){this.frecuencia=frecuencia;}
+        }
